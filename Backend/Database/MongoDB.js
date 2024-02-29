b40d1fc0ff6a33753e833f7bba8333e46cb3f2c5
Agukwe Henry <h.agukwe@yahoo.com>
2024-02-29 16:40:28 +0100

    function to connect the database
    -imported mongoose from package file
    
    - created a function to connect to database and retry connecting every
      5secs if connection fails

---
+++ Backend/Database/MongoDB.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose"
+ 
+mongoose.set('strictQuery', false);
+
+const dbConnect = async (db) => {
+
+  try {
+    const con = await mongoose.connect(db);
+    console.log(`Database connected with ${con.connection.host}`);
+  } catch (err) {
+    console.log(err);
+    setTimeout(dbConnect, 5000)
+  }
+};
+
+export default dbConnect;
