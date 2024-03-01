ab119f942dbce6c5fe4be440fe9005b52774acaf
TheSoftNode <thesoftnode@gmail.com>
2024-03-01 14:08:18 +0100

    Add global default error handlers
    - Added endpoint to handle uncaughtExceptions errors
    - Added endpoint to handle unhandled Rejections errors

--- Backend/Startups/Server.js
+++ Backend/Startups/Server.js
@@ -2,6 +2,13 @@ import dotenv from "dotenv";
 import dbConnect from "../Database/MongoDB.js";
 import app from "./App.js";
 
+// Handle uncaughtException
+process.on("uncaughtException", (err) => {
+  console.log("UNCAUGHT EXCEPTION! 💥 Shutting down...");
+  console.log(err.name, err.message);
+  process.exit(1);
+});
+
 dotenv.config({ path: "./config/config.env" });
 
 // Bring in the connection string
@@ -16,5 +23,15 @@ const port = process.env.PORT || 3000;
 // Listen to the port
 const server = app.listen(port, () => {
   console.log(`App running on port ${port}...`);
-  //   dbConnect(DB_storage);
+  dbConnect(DB_storage);
+});
+
+// Handle unhandled Rejections
+process.on("unhandledRejection", (err) => {
+  console.log("UNHANDLED REJECTION! 💥 Shutting down...");
+  console.log(err.name, err.message);
+
+  server.close(() => {
+    process.exit(1);
+  });
 });
