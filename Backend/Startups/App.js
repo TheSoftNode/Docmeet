bb655eed991e965beeccaf208828d3a9766a7a1f
TheSoftNode <thesoftnode@gmail.com>
2024-03-05 11:04:43 +0100

    The doctor and auth routes were mounted on the app

--- Backend/Startups/App.js
+++ Backend/Startups/App.js
@@ -19,11 +19,11 @@ app.use(express.json({ limit: "10kb" }));
 
 mountedRoutes(app);
 
-// app.all("*", (req, res, next) => {
-//   const err = new Error(`Can't find ${req.originalUrl} in this server!`);
-//   err.statusCode = 404;
-//   next(err);
-// });
+app.all("*", (req, res, next) => {
+  const err = new Error(`Can't find ${req.originalUrl} in this server!`);
+  err.statusCode = 404;
+  next(err);
+});
 
 app.use(globalErrorHandler);
 
