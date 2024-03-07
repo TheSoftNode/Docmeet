b655b432bd92f96ab1bb202fac00141ae1a8d60f
TheSoftNode <thesoftnode@gmail.com>
2024-03-07 08:31:53 +0100

    Add options for setting options if necessary

--- Backend/services/GenericService.js
+++ Backend/services/GenericService.js
@@ -65,13 +65,19 @@ export const getOne = (Model, popOptions) =>
     });
   });
 
-export const getAll = (Model) =>
+export const getAll = (Model, options = null) =>
   catchAsync(async (req, res, next) => {
     // To allow for nested GET reviews on tour (hack)
     let filter = {};
+    // if (Model.modelName == "Doctor") {
+    //   filter = { isApproved: "approved" };
+    // }
     // if (req.params.tourId) filter = { tour: req.params.tourId };
 
-    const features = new APIFeatures(Model.find(filter), req.query)
+    const features = new APIFeatures(
+      Model.find(filter).setOptions(options),
+      req.query
+    )
       .filter()
       .sort()
       .limitFields()
