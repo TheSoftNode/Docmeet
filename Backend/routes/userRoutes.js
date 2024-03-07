50ac3d5ae8e8d587391d2b24463a60740561a115
TheSoftNode <thesoftnode@gmail.com>
2024-03-07 08:23:54 +0100

    Add review routes for reviews
    - implemented mergeParams feature to make reviews on doctors easier

--- Backend/routes/userRoutes.js
+++ Backend/routes/userRoutes.js
@@ -11,15 +11,19 @@ import {
   updateUser,
   updateUserRole,
 } from "../controllers/userController.js";
+import restrictTo from "../middlewares/roleManager.js";
+import { isAuthenticated } from "../middlewares/protectRoutes.js";
 
 const router = express.Router();
 
 // Ensure that all the routes below are authenticated
+router.use(isAuthenticated);
 router.get("/me", getMe, getUser);
 router.patch("/update-me", updateMe);
 router.delete("/delete-me", deleteMe);
 
 // Restrict the endpoints below to admin access only
+router.use(restrictTo("admin"));
 router.route("/").get(getAllUsers).post(createUser);
 router.route("/:id").get(getUser).patch(updateUser).delete(deleteUser);
 
