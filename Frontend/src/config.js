e28211edea6d7e1a90ede9106ae6da6750b8163b
TheSoftNode <thesoftnode@gmail.com>
2024-03-28 16:08:28 +0100

    Modified config

--- Frontend/src/config.js
+++ Frontend/src/config.js
@@ -1,2 +1,3 @@
-export const BASE_URL = "http://localhost:7000/api/v1";
+export const BASE_URL = "https://docmeet-web-app-2.onrender.com";
+// export const BASE_URL = "http://localhost:7000/api/v1";
 export const token = localStorage.getItem("token");
